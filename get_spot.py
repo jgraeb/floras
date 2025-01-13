0a2d242f51184ce21d26a29afb018f55c6ad6787
Josefine G <51681591+jgraeb@users.noreply.github.com>
2025-01-13 08:09:26 -0800

    linting

--- get_spot.py
+++ get_spot.py
@@ -10,16 +10,21 @@ import textwrap as _tw
 import sys
 
 SPOT_VERSION: _ty.Final = '2.12'
-SPOT_SHA256: _ty.Final = '26ba076ad57ec73d2fae5482d53e16da95c47822707647e784d8c7cec0d10455'
+SPOT_SHA256: _ty.Final = (
+    '26ba076ad57ec73d2fae5482d53e16da95c47822707647e784d8c7cec0d10455'
+)
 SPOT_TARBALL: _ty.Final = f'spot-{SPOT_VERSION}.tar.gz'
-SPOT_URL: _ty.Final = ('http://www.lrde.epita.fr/dload/spot/'
-                    f'spot-{SPOT_VERSION}.tar.gz')
+SPOT_URL: _ty.Final = (
+    'http://www.lrde.epita.fr/dload/spot/'f'spot-{SPOT_VERSION}.tar.gz'
+)
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
-SPOT_PATH = os.path.join(FILE_PATH,f'spot-{SPOT_VERSION}')
+SPOT_PATH = os.path.join(FILE_PATH, f'spot-{SPOT_VERSION}')
 ENV_PATH = sys.prefix
+
 
 def is_hash_correct(filename):
     return SPOT_SHA256 == hashlib.sha256(open(filename, 'rb').read()).hexdigest()
+
 
 def fetch_spot():
     filename = SPOT_TARBALL
@@ -27,10 +32,13 @@ def fetch_spot():
     untar(filename)
     make_spot()
 
-def fetch(url,filename):
+
+def fetch(url, filename):
     if os.path.isfile(filename):
         if not is_hash_correct(filename):
-            raise RuntimeError(f'File `{filename}` already present but has unexpected hash.')
+            raise RuntimeError(
+                f'File `{filename}` already present but has unexpected hash.'
+            )
         print(
             f'File `{filename}` already present.')
         return
@@ -42,8 +50,9 @@ def fetch(url,filename):
                 '`urllib.request.urlopen` returned `None` '
                 'when attempting to open the URL:  '
                 f'{url}')
-    except urllib.error.URLError as url_error:
-        raise RuntimeError(_tw.dedent(f'An exception was raised when attempting to open the URL: {url}'))
+    except urllib.error.URLError:
+        raise RuntimeError(_tw.dedent(
+            f'An exception was raised when attempting to open the URL: {url}'))
     with response, open(filename, 'wb') as f:
         f.write(response.read())
     print(
@@ -55,11 +64,13 @@ def fetch(url,filename):
     if not is_hash_correct(filename):
         raise RuntimeError(f'Downloaded file `{filename}` has unexpected hash.')
 
+
 def untar(filename):
     print(f'++ unpack: {filename}')
     with tarfile.open(filename) as tar:
         tar.extractall()
     print('-- done unpacking.')
+
 
 def make_spot():
     """Compile spot."""
@@ -68,7 +79,8 @@ def make_spot():
     print(f'running: `{cmd}` in {path}')
     subprocess.check_call(cmd, shell=True, cwd=path)
     subprocess.check_call(['make'], cwd=path)
-    subprocess.check_call(['make','install'], cwd=path)
+    subprocess.check_call(['make', 'install'], cwd=path)
+
 
 if __name__ == '__main__':
     fetch_spot()
